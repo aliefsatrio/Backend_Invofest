bc5b73ca46f5be58eb7a114454014763d46718e0
aliefsatrio <alifsatrio757@gmail.com>
2026-05-22 22:40:12 +0700

    Backend

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,16 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import pembicaraRoutes from "./routes/pembicaraRoute.js";
 
 const app = express();
-const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Ini adalah api untuk invofest");
+    res.send("Ini adalah api untuk invofest 🚀");
 });
 
 app.use("/events", eventRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/pembicara", pembicaraRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
+export default app;
